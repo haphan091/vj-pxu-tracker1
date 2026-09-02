04b55f8d444a3bde4dd3fb9dcf05726495f06194
haphan091 <38711715+haphan091@users.noreply.github.com>
2026-09-02 08:54:12 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Cập nhật AGP lên bản tương thích JDK 17
+        classpath 'com.android.tools.build:gradle:8.2.2'
+        // Cập nhật Kotlin lên phiên bản đã xóa bỏ HasConvention
+        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22'
+    }
 }
